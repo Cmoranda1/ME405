3d7c59a3f271bff82aa4830e924bb67047009da1
Cmoranda1 <Chrismoranda@gmail.com>
2019-01-08 14:39:21 -0800

    Motor_Lab0

---
+++ motor_moranda_mueller.py
@@ -0,0 +1,54 @@
+# main.py -- put your code here!
+import pyb
+''' @file main.py
+There must be a docstring at the beginning of a Python
+source file with an @file [filename] tag in it! '''
+class MotorDriver:
+	''' This class implements a motor driver for the
+	ME405 board. '''
+	def __init__ (self):
+		''' Creates a motor driver by initializing GPIO
+		pins and turning the motor off for safety. '''
+	
+		#initialize pins
+		self.EN_A = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP)
+		self.IN1A = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
+		self.IN2A = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
+		
+		#set all pins low
+		self.EN_A.low()
+		self.IN1A.low()
+		self.IN2A.low()
+		
+		self.tim3 = pyb.Timer(3, freq=20000)
+		self.ch1 = self.tim3.channel(1, pyb.Timer.PWM, pin=self.IN1A)
+		self.ch2 = self.tim3.channel(2, pyb.Timer.PWM, pin=self.IN2A)
+		
+		print ('Creating a motor driver')
+	def set_duty_cycle (self, level):
+		''' This method sets the duty cycle to be sent
+		to the motor to the given level. Positive values
+		cause torque in one direction, negative values
+		in the opposite direction.
+		@param level A signed integer holding the duty
+		cycle of the voltage sent to the motor '''
+		
+		print ('Setting duty cycle to ' + str (level))
+
+		self.EN_A.high()
+		#direction
+		if level>=0:
+			self.IN1A.low()
+			self.ch2.pulse_width_percent(abs(level))
+			
+		else:
+			self.IN2A.low()
+			self.ch1.pulse_width_percent(level)
+			
+if __name__ == '__main__':
+	moe = MotorDriver()
+	moe.set_duty_cycle(80)
+	
+	moe.set_duty_cycle(-80)
+
+	moe.set_duty_cycle(0)
