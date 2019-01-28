0684a2a9754da89fddf382aadcbfa60f4c3400c0
Cmoranda1 <Chrismoranda@gmail.com>
2019-01-28 11:34:07 -0800

    Update closed_loop.py
    Working Code

--- lab2_220119/closed_loop.py
+++ lab2_220119/closed_loop.py
@@ -2,7 +2,7 @@ class Closed_Loop:
 	def __init__(self):
 		self.setpoint = 0
 		self.propgain = 0
-		print("Creating Closed Loop System")
+		#print("Creating Closed Loop System")
 		
 	def control(self, curr_pos):
 		#print("distance from setpoint:" + str(self.setpoint - curr_pos))
@@ -14,8 +14,8 @@ class Closed_Loop:
 		
 	def set_setpoint(self, setpoint):
 		self.setpoint = setpoint
-		print("setpoint is set to: " + str(self.setpoint))
+		#print("setpoint is set to: " + str(self.setpoint))
 		
 	def set_cont_gain(self, cont_gain):
 		self.propgain = cont_gain
-		print("Gain is set to: " + str(self.propgain))
+		#print("Gain is set to: " + str(self.propgain))
